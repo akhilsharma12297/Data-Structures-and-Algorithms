1ed8fdce253b4b2ce15d440809636f2fa54a721f
akhilsharma12297 <akhil.sharma12297@gmail.com>
2019-07-07 18:30:25 +0530

    Code

--- Recursion/No_of_Inverstion.java
+++ Recursion/No_of_Inverstion.java
@@ -3,36 +3,59 @@ package Recursion;
 public class No_of_Inverstion {
 
 	public static void main(String[] args) {
+		int arr[] = new int[] { 1, 20, 6, 4, 5 };
 
+		System.out.println(inversion(arr));
 	}
 
 	public static int inversion(int[] arr) {
-
-		return CountInversion(arr, new int[arr.length], 0, arr.length - 1);
+		return CountInversion(arr, 0, arr.length - 1);
 	}
 
-	private static int CountInversion(int[] arr, int[] temp, int low, int high) {
+	public static int CountInversion(int arr[], int left, int right) {
+		int mid, inv_count = 0;
+		if (right > left) {
 
-		int inversionCtr = 0;
+			mid = (right + left) / 2;
 
-		while (high > low) {
-			int mid = (low + high) / 2;
-			inversionCtr = CountInversion(arr, temp, low, mid);
+			inv_count = CountInversion(arr, left, mid);
 
-			inversionCtr += CountInversion(arr, temp, mid + 1, high);
+			inv_count += CountInversion(arr, mid + 1, right);
 
-			inversionCtr += MergeSorted(arr, temp, low, high);
+			inv_count += MergeSorted(arr, left, mid + 1, right);
+		}
+		return inv_count;
+	}
+
+	private static int MergeSorted(int[] arr, int left, int mid, int right) {
+
+		int[] temp = new int[arr.length];
+		int i, j, k;
+		int inv_count = 0;
+
+		i = left;
+		j = mid;
+		k = left;
+		while ((i <= mid - 1) && (j <= right)) {
+			if (arr[i] <= arr[j]) {
+				temp[k++] = arr[i++];
+			} else {
+				temp[k++] = arr[j++];
+
+				inv_count = inv_count + (mid - i);
+			}
 		}
 
-		return inversionCtr;
+		while (i <= mid - 1)
+			temp[k++] = arr[i++];
 
-	}
+		while (j <= right)
+			temp[k++] = arr[j++];
 
-	private static int MergeSorted(int[] arr, int[] temp, int low, int high) {
+		for (i = left; i <= right; i++)
+			arr[i] = temp[i];
 
-		
-		
-		return 0;
+		return inv_count;
 	}
 
 }
