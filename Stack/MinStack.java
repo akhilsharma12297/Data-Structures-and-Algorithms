9333cba54249f2370030a859160c8205e430b28b
akhilsharma12297 <akhil.sharma12297@gmail.com>
2019-07-10 17:19:05 +0530

    Code

--- Stack/MinStack.java
+++ Stack/MinStack.java
@@ -3,84 +3,46 @@ package Stack;
 import java.util.Stack;;
 
 public class MinStack {
+	static int minElement;
 
-	static int minEle;
+	static Stack<Integer> stack = new Stack<Integer>();
 
-	static Stack<Integer> s = new Stack<>();
-
-	static int getMin() {
-
-		if (s.isEmpty()) {
-			System.out.println("Stack is empty");
-			return -1;
-		}
-
-		return minEle;
-	}
-
-	static int pop() {
-
-		if (s.isEmpty()) {
-			System.out.println("Stack is empty");
-			return -1;
-		}
-
-		int val = s.pop();
-
-		if (val < minEle) {
-
-			int temp = minEle;
-			minEle = 2 * minEle - val;
-			return temp;
+	public static void push(int val) {
+		if (stack.isEmpty()) {
+			stack.add(val);
+			minElement = val;
 		} else {
-			return val;
-		}
-
-	}
-
-	static void push(int x) {
-
-		if (s.size() == 0) {
-			minEle = x;
-			s.push(x);
-		} else {
-			if (x < minEle) {
-				s.push(2 * x - minEle);
-				minEle = x;
+			if (val < minElement) {
+				stack.push(2 * val - minElement);
+				minElement = val;
+			} else {
+				stack.push(val);
 			}
 		}
+
 	}
 
-	public static int min_str_reversal(String s) {
+	public static int pop() {
 
-		int len = s.length();
-
-		if (len % 2 != 0)
+		if (stack.isEmpty()) {
+			System.out.println("Stack is Empty");
 			return -1;
+		} else {
+			int temp = stack.pop();
+			if (temp < minElement) {
+				int min = minElement;
+				minElement = 2 * minElement - temp;
+				return min;
+			}
+			return temp;
 
-		Stack<Character> stack = new Stack<>();
-
-		for (int i = 0; i < len; i++) {
-			char c = s.charAt(i);
-			if (c == '}' && !stack.empty()) {
-				if (stack.peek() == '{')
-					stack.pop();
-				else
-					stack.push(c);
-			} else
-				stack.push(c);
 		}
-
-		int red_len = stack.size();
-
-		int n = 0;
-		while (!stack.empty() && stack.peek() == '{') {
-			stack.pop();
-			n++;
-		}
-
-		return (red_len / 2 + n % 2);
 
 	}
 
+	public static int getMin() {
+		if (stack.size() == 0)
+			return -1;
+		return minElement;
+	}
 }
