ffd1a69f042de197e4564be7ed868f8003a2dbd3
akhilsharma12297 <akhil.sharma12297@gmail.com>
2019-07-12 13:08:47 +0530

    Code

--- DP/Friend_paring_problem.java
+++ DP/Friend_paring_problem.java
@@ -9,6 +9,18 @@ public class Friend_paring_problem {
 	}
 
 	public static void func(int n) {
+
+		int[] dp = new int[n + 1];
+
+		for (int i = 0; i <= n; i++) {
+			if (i <= 2)
+				dp[i] = i;
+			else
+				dp[i] = dp[i - 1] + dp[i - 2] * (i - 1);
+		}
+
+		System.out.println(dp[n]);
+
 	}
 
 }
