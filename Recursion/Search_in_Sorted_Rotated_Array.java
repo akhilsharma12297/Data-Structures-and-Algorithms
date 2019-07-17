f6af6f74dfe0580c3f0cd1be666feaae2d38c312
akhilsharma12297 <akhil.sharma12297@gmail.com>
2019-07-17 15:07:13 +0530

    Code

---
+++ Recursion/Search_in_Sorted_Rotated_Array.java
@@ -0,0 +1,46 @@
+package Recursion;
+
+public class Search_in_Sorted_Rotated_Array {
+
+	public static void main(String[] args) {
+
+		int arr[] = { 4, 5, 6, 7, 8, 9, 1, 2, 3 };
+		int key = 6;
+
+		System.out.println(search(arr, key));
+
+	}
+
+	public static int search(int[] arr, int key) {
+
+		return searchHelper(arr, 0, arr.length - 1, key);
+
+	}
+
+	private static int searchHelper(int[] arr, int low, int high, int key) {
+
+		if (low > high) {
+			return -1;
+		}
+
+		int mid = (low + high) / 2;
+
+		if (arr[mid] == key) {
+			return mid;
+		}
+
+		if (arr[low] <= arr[mid]) {
+			if (key >= arr[low] && key <= arr[mid]) {
+				return searchHelper(arr, low, mid - 1, key);
+			}
+			return searchHelper(arr, mid + 1, high, key);
+		}
+
+		if (key >= arr[mid] && key >= arr[high]) {
+			return searchHelper(arr, mid + 1, high, key);
+		}
+
+		return searchHelper(arr, low, mid - 1, key);
+
+	}
+}
