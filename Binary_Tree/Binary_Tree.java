17a278ce56fa249e57e3dbbee496b3c63d78abb5
akhilsharma12297 <akhil.sharma12297@gmail.com>
2019-07-18 16:55:49 +0530

    Code

--- Binary_Tree/Binary_Tree.java
+++ Binary_Tree/Binary_Tree.java
@@ -904,7 +904,7 @@ public class Binary_Tree {
 
 		mypair.isBST = lp.isBST && rp.isBST && node.data >= lp.max && node.data < rp.min;
 		mypair.max = Math.max(node.data, Math.max(lp.max, rp.max));
-		mypair.min = Math.max(node.data, Math.max(lp.min, rp.min));
+		mypair.min = Math.min(node.data, Math.min(lp.min, rp.min));
 
 		return mypair;
 
