c771f62a40db4c432e1de1e07619539c18a2eab4
akhilsharma12297 <akhil.sharma12297@gmail.com>
2019-08-05 14:53:45 +0530

    Code

--- Recursion/Candy_Distribution.java
+++ Recursion/Candy_Distribution.java
@@ -1,51 +1,79 @@
 package Recursion;
 
-import java.util.*;
-
 public class Candy_Distribution {
 
-	public static int code(ArrayList<Integer> ratings) {
+	public static void code(int[] ratings) {
 
-		if (ratings == null || ratings.size() == 0)
-			return 0;
+		if (ratings == null || ratings.length == 0)
+			return;
 
-		int[] candies = new int[ratings.size()];
+		int[] candies = new int[ratings.length];
 
 		candies[0] = 1;
 
-		for (int i = 1; i < ratings.size(); i++) {
+		for (int i = 1; i < ratings.length; i++) {
 
-			if (ratings.get(i) > ratings.get(i - 1))
-
+			if (ratings[i] > ratings[i - 1])
 				candies[i] = candies[i - 1] + 1;
 			else {
-
 				candies[i] = 1;
 			}
 		}
 
-		int result = candies[ratings.size() - 1];
+		int result = candies[candies.length - 1];
 
-		for (int i = ratings.size() - 2; i >= 0; i--) {
+		for (int i = ratings.length - 2; i >= 0; i--) {
 			int cur = 1;
-			if (ratings.get(i) > ratings.get(i + 1))
-
+			if (ratings[i] > ratings[i + 1])
 				cur = candies[i + 1] + 1;
-
 			result += Math.max(cur, candies[i]);
 
 			candies[i] = cur;
+
+			System.out.print(candies[i] + " ");
+		}
+		System.out.println();
+		System.out.print(result);
+	}
+
+	public static void ans(int[] arr) {
+
+		int n = arr.length;
+
+		int candies[] = new int[n];
+		candies[0] = 1;
+
+		for (int i = 1; i < n; i++) {
+			if (candies[i] == 0) {
+				candies[i] = 1;
+			}
+			if (arr[i] > arr[i - 1]) {
+				candies[i] = candies[i - 1] + 1;
+			}
 		}
 
-		return result;
+		for (int i = n - 1; i > 0; i--) {
+			if (arr[i - 1] > arr[i] && candies[i - 1] <= candies[i]) {
+				candies[i - 1] = candies[i] + 1;
+			}
+		}
+		long sum = 0;
+		for (int i = 0; i < n; i++) {
+			sum += candies[i];
+
+			System.out.print(candies[i] + " ");
+		}
+		System.out.println();
+		System.out.print(sum);
+
 	}
 
 	public static void main(String[] args) {
-		ArrayList<Integer> ratings = new ArrayList<Integer>();
-		ratings.add(1);
-		ratings.add(2);
-		ratings.add(2);
-		System.out.println(code(ratings));
+
+		int[] arr = { 6, 3, 4, 4, 3, 2, 1 };
+		int[] arr2 = { 8, 1, 2, 2, 3, 4, 3, 2, 1 };
+
+		ans(arr2);
 
 	}
 }
